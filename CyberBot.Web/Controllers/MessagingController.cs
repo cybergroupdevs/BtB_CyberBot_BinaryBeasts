f1fed67af0e9c0469c55173a7b0218a9c3857014
Akshat Agarwal <akag2411@gmail.com>
2019-12-10 21:08:51 +0530

    Replaced with final code - chatbot ui and blank api project

---
+++ CyberBot.Web/Controllers/MessagingController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CyberBot.Web.Controllers
+{
+    public class MessagingController : Controller
+    {
+        // GET: Messaging
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
